0d0fd92f16b732ff557a15c9d246c2ad592c0213
yourOnlyCode <59841414+yourOnlyCode@users.noreply.github.com>
2022-02-17 12:18:30 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+
+
+<script>
+	import PdfViewer from 'svelte-pdf';
+	let title = 'MINIVERSE'
+</script>
+
+<div class="header-container">
+	<div class="logo-container">
+		<img class="logo" src="favicon.png" alt="globe"/>
+	</div>
+
+	<div class="title-container">
+		<h1 class="title">{title}</h1>
+	</div>
+</div>	
+
+<PdfViewer url='./manuscripts/1.pdf' />
