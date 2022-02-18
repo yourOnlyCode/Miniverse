1576f3a32de9ed4663452e9526bebd5ea4e912aa
yourOnlyCode <59841414+yourOnlyCode@users.noreply.github.com>
2022-02-18 16:00:46 -0500

    added a config file

---
+++ svelte.config.cjs
@@ -0,0 +1,13 @@
+const adapter = require('@sveltejs/adapter-netlify');
+module.exports = {
+	kit: {
+		adapter: adapter(), // currently the adapter does not take any options
+		target: '#svelte',
+		prerender: {
+			crawl: true,
+			enabled: true,
+			force: true,
+			pages: ['*'],
+		},
+	}
+};
