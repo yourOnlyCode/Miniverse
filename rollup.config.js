4e20c9df32ab5f281b0b980adbfb101d9098fbd7
yourOnlyCode <59841414+yourOnlyCode@users.noreply.github.com>
2022-02-18 16:48:08 -0500

    got rid of duplicate export default in config

--- rollup.config.js
+++ rollup.config.js
@@ -6,16 +6,6 @@ import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 import adapter from '@sveltejs/adapter-netlify';
 
-export default {
-	kit: {
-		adapter: adapter({
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
-			split: false
-		})
-	}
-};
-
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
@@ -40,6 +30,13 @@ function serve() {
 }
 
 export default {
+	kit: {
+		adapter: adapter({
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		})
+	},
 	input: 'src/main.js',
 	output: {
 		sourcemap: true,
