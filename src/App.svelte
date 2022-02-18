afe296e5fe868625d777c23e7309c5d871f1c3b6
yourOnlyCode <59841414+yourOnlyCode@users.noreply.github.com>
2022-02-18 15:23:46 -0500

    testing Netlify forms

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 
-
-
 <script>
 	import PdfViewer from 'svelte-pdf';
 	let title = 'MINIVERSE'
+
+	function openPopup() {
+		window.location.hash = 'openModal';
+	}
+
+	window.onLoad = openPopup;
 </script>
 
 <div class="header-container">
@@ -16,7 +20,44 @@
 	</div>
 
 	<div class="disclaimer-container">
-		<h2 class="disclaimer">public login unavailable</h2>
+		<h2 class="disclaimer">
+			<a href="#openModal">LOGIN</a>
+			<div id="openModal" class="modalDialog">
+			<div class="popup-container">
+				<a href="#close" style="color:black" title="Close" class="close">X</a>
+				<p style="color:black">Login not yet available for public.</p>
+				<p style="color:black">Get notified for public launch:</p>
+
+				<!-- <label for="email">Email</label>
+				<input type="text" placeholder="Enter Email" name="email" required>
+
+				<div class="clearfix">
+					<button type="submit" class="signupbtn">Sign Up</button>
+				</div> -->
+
+				<!--NETLIFY SUBMITTION Hidden field so form helper can see it-->
+				<form name="test" netlify netlify-honeypot="bot-field" hidden>
+					<input type="text" name="name"/>
+					<input type="email" name="email"/>
+					<textarea name="message"></textarea>
+				</form>
+
+				<!--Svelte form component-->
+				<form name="contact" netlify>
+					<p>
+					  <label>Name <input type="text" name="name" /></label>
+					</p>
+					<p>
+					  <label>Email <input type="email" name="email" /></label>
+					</p>
+					<p>
+					  <button type="submit">Send</button>
+					</p>
+				  </form>
+
+			</div>
+			</div>
+		</h2>
 	</div>
 </div>	
 
