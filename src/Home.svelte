5deef0ba9495218601dbee3ca0c602e4783c6eba
yourOnlyCode <59841414+yourOnlyCode@users.noreply.github.com>
2022-02-19 11:34:24 -0500

    added a hash router, don't like it though, switch to a svelte-navigator

---
+++ src/Home.svelte
@@ -0,0 +1,37 @@
+
+
+<label class="btn" for="modal-1">Login</label>
+<input class="modal-state" id="modal-1" type="checkbox" />
+<div class="modal">
+    <p style="color:black">Login not yet available for public.</p>
+    <p style="color:black">Get notified for public launch:</p>
+
+    <a href="https://forms.gle/VPKkh41koqduevZW8">Click Here</a>
+</div>
+
+<style>
+.modal {
+    display: flex;
+    width: 66%;
+    justify-content: center;
+  opacity: 0;
+  visibility: hidden;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  text-align: left;
+  background: rgba(0,0,0, .9);
+  transition: opacity .25s ease;
+}    
+
+.modal-state {
+  display: none;
+}
+
+.modal-state:checked + .modal {
+  opacity: 1;
+  visibility: visible;
+}
+</style>
---
+++ src/Mattysbox.svelte
@@ -0,0 +1,65 @@
+<script context="module">
+
+	export const prerender = true
+
+	import PdfViewer from 'svelte-pdf';
+    import Router, {location, link} from 'svelte-spa-router';
+
+	
+	let title = 'MINIVERSE'
+</script>
+
+<div class="header-container">
+
+        <div class="logo-container">
+            <img class="logo" src="favicon.png" alt="globe"/>
+        </div>
+
+
+        <div class="title-container">
+            <h1 class="title">{title}</h1>
+        </div>  
+
+	<div class="disclaimer-container">
+		<h2 class="disclaimer">
+			<a href="#openModal">LOGIN</a>
+			<div id="openModal" class="modalDialog">
+			<div class="popup-container">
+				<a href="#close" style="color:black" title="Close" class="close">X</a>
+				<p style="color:black">Login not yet available for public.</p>
+				<p style="color:black">Get notified for public launch:</p>
+
+				<!-- <label for="email">Email</label>
+				<input type="text" placeholder="Enter Email" name="email" required>
+
+				<div class="clearfix">
+					<button type="submit" class="signupbtn">Sign Up</button>
+				</div> -->
+
+				<!--Svelte form component-->
+				<!-- <form name="contact" netlify>
+					<p>
+					  <label>Your Name: <input type="text" name="name" /></label>   
+					</p>
+					<p>
+					  <label>Your Email: <input type="email" name="email" /></label>
+					</p>
+					<p>
+					  <button type="submit">Send</button>
+					</p>
+				</form> -->
+
+				<a href="https://forms.gle/VPKkh41koqduevZW8">Click Here</a>
+
+			</div>
+			</div>
+		</h2>
+	</div>
+
+</div>	
+
+<div class="box-container">
+	<h3 class="box">MATTY's BOX</h3>
+</div>
+
+<PdfViewer url='./manuscripts/1.pdf' />
